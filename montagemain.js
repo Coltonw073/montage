f00d64d2044844c358c38bec5bb42d8f32355fd8
Coltonw073 <51644059+Coltonw073@users.noreply.github.com>
2022-07-13 10:30:00 -0400

    Add files via upload

---
+++ montagemain.js
@@ -0,0 +1,460 @@
+var plst = [
+  'CGhETgYetgg',
+  'mFNCWodwkyD',
+  '|}5u$jlRM!}',
+  'N+0al_C506I',
+  'kLaEO1rjbuN'
+]
+var pdct = {
+  'CGhETgYetgg': {
+    'name': 'TSM FTX Scrubz',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=OUowC_BVhwA',
+        'title': 'Tip Toe 👣'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=Pw3w08Lccfo',
+        'title': 'Lingo🎶'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=IWfoOVuyyQI',
+        'title': 'Realize 🤯'
+      },
+      {
+        'title': 'Valentina💃',
+        'url': 'https://www.youtube.com/watch?v=BJAnAHAIa_c'
+      },
+      {
+        'title': 'No Hoodie❌',
+        'url': 'https://www.youtube.com/watch?v=s7LwLR7PtAg'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=WnyGnZucZ0I',
+        'title': 'So I Joined _____ 🤯'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=bpyCnMjaJ4s',
+        'title': 'Joined @TSM | Messi⚽'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=LrFRG8k30ZE',
+        'title': 'Not The Same ❌ | Scrubz'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=gUh3qYs1GfY',
+        'title': 'Outta Time'
+      },
+      {
+        'title': 'SHOOT🎯',
+        'url': 'https://www.youtube.com/watch?v=BJAnAHAIa_c'
+      }
+    ]
+  },
+  'mFNCWodwkyD': {
+    'name': 'Parm',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=jW6uLaAHSjY',
+        'title': 'Dreaming 💭 + Best 60FPS Player Switches To 144FPS'
+      },
+      {
+        'title': 'HIM 👑 + Best Controller Settings For AIMBOT/Piece Control🧩',
+        'url': 'https://www.youtube.com/watch?v=cnmgs0rmzVY'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=AsRZDjHJ3K8',
+        'title': 'Bad Boy 👿 (Joined Chronic! 🌟)'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=BdCQPsUXp84',
+        'title': 'Mannequin 👻 Parm Highlights #1'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=ZDM4cDJf5aU',
+        'title': 'Calling My Phone 📞 | Parm Highlights #2'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=7gLVWI4cu98',
+        'title': 'Cold ❄️ | Parm Highlights #3'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=pOlAKn9JHno',
+        'title': 'AP 💉 | Parm Highlights #4'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=31-jWVBsAC8',
+        'title': 'Catch Me Outside 😈 Parm Highlights #5'
+      }
+    ]
+  },
+  '|}5u$jlRM!}': {
+    'name': 'Keeny',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=XfYccU4LNB8',
+        'title': 'Bad Boy 🥀'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=p4JMaO_dKis',
+        'title': 'Like A Boss 😤'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=FH9278_JeYw',
+        'title': 'No Hoodie ❌'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=u03DucPr7sY',
+        'title': 'Cold ❄️ (Joined @Optimal Ambition)'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=kD6ycMuRw8c',
+        'title': 'Touchdown 🏈'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=0QWDzM742Qg',
+        'title': 'Lucid Dreams 🌧'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=NKKTaHWijvk',
+        'title': 'Sauce It Up 🛸'
+      }
+    ]
+  },
+  'N+0al_C506I': {
+    'name': 'FaZe Sway',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=BuehAP5GEAU',
+        'title': 'Whoopty 🤘 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=bKk7fr-Xifo',
+        'title': 'Suede 👞 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=2QtwKwuXfOg', 'title': 'NASCAR 🏎️ | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=ViguxotJHZ8',
+        'title': 'Bad Boy 🥀 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=8e_S3qQBzNY',
+        'title': 'Calling My Phone 📞 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=qhYKtMKtm-o',
+        'title': 'Like a Boss 🐐 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=95k19mf2AuA',
+        'title': 'Rosetta 🌹 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=2p_GLaf8Xbc',
+        'title': 'Opps 🔫 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=tp_trR_-q5o',
+        'title': 'Messi ⚽️ ft. *MY BEST CLIPS* | FaZe Sway'
+      }
+    ]
+  },
+  'kLaEO1rjbuN': {
+    'name': 'Deyyszn',
+    'vids': [
+      {
+        "url": "https://www.youtube.com/watch?v=hlNy2wGzF60",
+        "title": "Dreaming 💭 | Deyy Highlights #16"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=U8FUyb_vxh0",
+        "title": "Nascar 🏎️ | Deyy Highlights #17"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=n-0gPs5oFDU",
+        "title": "Lingo 🎶 | Deyy Highlights #18"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=9K1NwrMevK0",
+        "title": "FAKE LOVE 💔 | Deyy Highlights #19"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=v87Upt-9AY8",
+        "title": "No Hoodie ❌ | Deyy Highlights #20"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=4PxUIxhhw1U",
+        "title": "Barbie 👸 (Best Controller Settings)"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=vpjC_SsVnYo",
+        "title": "No Clue ❓"
+      }
+    ]
+  }
+}
+var plst2 = [
+  'CGhETgYetgg',
+  'mFNCWodwkyD',
+  '|}5u$jlRM!}',
+  'N+0al_C506I',
+  'kLaEO1rjbuN'
+]
+var pdct2 = {
+  'CGhETgYetgg': {
+    'name': 'TSM FTX Scrubz',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=OUowC_BVhwA',
+        'title': 'Tip Toe 👣'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=Pw3w08Lccfo',
+        'title': 'Lingo🎶'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=IWfoOVuyyQI',
+        'title': 'Realize 🤯'
+      },
+      {
+        'title': 'Valentina💃',
+        'url': 'https://www.youtube.com/watch?v=BJAnAHAIa_c'
+      },
+      {
+        'title': 'No Hoodie❌',
+        'url': 'https://www.youtube.com/watch?v=s7LwLR7PtAg'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=WnyGnZucZ0I',
+        'title': 'So I Joined _____ 🤯'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=bpyCnMjaJ4s',
+        'title': 'Joined @TSM | Messi⚽'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=LrFRG8k30ZE',
+        'title': 'Not The Same ❌ | Scrubz'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=gUh3qYs1GfY',
+        'title': 'Outta Time'
+      },
+      {
+        'title': 'SHOOT🎯',
+        'url': 'https://www.youtube.com/watch?v=BJAnAHAIa_c'
+      }
+    ]
+  },
+  'mFNCWodwkyD': {
+    'name': 'Parm',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=jW6uLaAHSjY',
+        'title': 'Dreaming 💭 + Best 60FPS Player Switches To 144FPS'
+      },
+      {
+        'title': 'HIM 👑 + Best Controller Settings For AIMBOT/Piece Control🧩',
+        'url': 'https://www.youtube.com/watch?v=cnmgs0rmzVY'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=AsRZDjHJ3K8',
+        'title': 'Bad Boy 👿 (Joined Chronic! 🌟)'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=BdCQPsUXp84',
+        'title': 'Mannequin 👻 Parm Highlights #1'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=ZDM4cDJf5aU',
+        'title': 'Calling My Phone 📞 | Parm Highlights #2'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=7gLVWI4cu98',
+        'title': 'Cold ❄️ | Parm Highlights #3'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=pOlAKn9JHno',
+        'title': 'AP 💉 | Parm Highlights #4'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=31-jWVBsAC8',
+        'title': 'Catch Me Outside 😈 Parm Highlights #5'
+      }
+    ]
+  },
+  '|}5u$jlRM!}': {
+    'name': 'Keeny',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=XfYccU4LNB8',
+        'title': 'Bad Boy 🥀'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=p4JMaO_dKis',
+        'title': 'Like A Boss 😤'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=FH9278_JeYw',
+        'title': 'No Hoodie ❌'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=u03DucPr7sY',
+        'title': 'Cold ❄️ (Joined @Optimal Ambition)'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=kD6ycMuRw8c',
+        'title': 'Touchdown 🏈'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=0QWDzM742Qg',
+        'title': 'Lucid Dreams 🌧'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=NKKTaHWijvk',
+        'title': 'Sauce It Up 🛸'
+      }
+    ]
+  },
+  'N+0al_C506I': {
+    'name': 'FaZe Sway',
+    'vids': [
+      {
+        'url': 'https://www.youtube.com/watch?v=BuehAP5GEAU',
+        'title': 'Whoopty 🤘 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=bKk7fr-Xifo',
+        'title': 'Suede 👞 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=2QtwKwuXfOg', 'title': 'NASCAR 🏎️ | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=ViguxotJHZ8',
+        'title': 'Bad Boy 🥀 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=8e_S3qQBzNY',
+        'title': 'Calling My Phone 📞 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=qhYKtMKtm-o',
+        'title': 'Like a Boss 🐐 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=95k19mf2AuA',
+        'title': 'Rosetta 🌹 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=2p_GLaf8Xbc',
+        'title': 'Opps 🔫 | FaZe Sway'
+      },
+      {
+        'url': 'https://www.youtube.com/watch?v=tp_trR_-q5o',
+        'title': 'Messi ⚽️ ft. *MY BEST CLIPS* | FaZe Sway'
+      }
+    ]
+  },
+  'kLaEO1rjbuN': {
+    'name': 'Deyyszn',
+    'vids': [
+      {
+        "url": "https://www.youtube.com/watch?v=hlNy2wGzF60",
+        "title": "Dreaming 💭 | Deyy Highlights #16"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=U8FUyb_vxh0",
+        "title": "Nascar 🏎️ | Deyy Highlights #17"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=n-0gPs5oFDU",
+        "title": "Lingo 🎶 | Deyy Highlights #18"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=9K1NwrMevK0",
+        "title": "FAKE LOVE 💔 | Deyy Highlights #19"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=v87Upt-9AY8",
+        "title": "No Hoodie ❌ | Deyy Highlights #20"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=4PxUIxhhw1U",
+        "title": "Barbie 👸 (Best Controller Settings)"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=vpjC_SsVnYo",
+        "title": "No Clue ❓"
+      }
+    ]
+  }
+}
+var plst3 = [
+  'CGhETgYetgg',
+  'mFNCWodwkyD',
+  '|}5u$jlRM!}',
+  'N+0al_C506I',
+  'kLaEO1rjbuN'
+]
+function rvid() {
+  if (plst.length == 0) {
+    a = 0
+    while (a < plst2.length) {
+      plst.push(plst2[a])
+      a = a + 1
+    }
+  }
+  pl = Math.floor(Math.random() * (plst.length))
+  pid = plst[pl]
+  pnm = pdct[plst[pl]]['name']
+  if (pdct[plst[pl]]['vids'].length == 0) {
+    plst2.splice(plst2.indexOf(pid), 1);
+    plst.splice(plst.indexOf(pid), 1)
+    if (plst2.length != 0) {
+      rvid();
+      return;
+    }
+    if (plst2.length == 0) {
+      a = 0
+      plst = []
+      pdct = {}
+      plst2 = []
+      while (a < plst3.length) {
+        plst.push(plst3[a])
+        plst2.push(plst3[a])
+        a = a + 1
+      }
+      a = 0
+      while (a < plst.length) {
+        pobj = pdct2[plst[a]]
+        pnm = pobj['name']
+        vds = pobj['vids']
+        pdct[plst[a]] = {'name': pnm, 'vids': vds}
+        a = a + 1
+      }
+      rvid();
+      return;
+    }
+  }
+  vidnum = Math.floor(Math.random() * (pdct[plst[pl]]['vids'].length))
+  vidnm = pdct[plst[pl]]['vids'][vidnum]['title']
+  vurl = pdct[plst[pl]]['vids'][vidnum]['url']
+  pdct[plst[pl]]['vids'].splice(vidnum, 1)
+  plst.splice(plst.indexOf(pid), 1)
+  document.getElementById('title').innerHTML = 'Generating Montage...'
+  document.getElementById('player').innerHTML = ''
+  document.getElementById('mainimg').removeAttribute
+  document.getElementById('mainimg').style.visibility = 'hidden'
+  setTimeout(function() {
+    document.getElementById('title').innerHTML = vidnm
+    document.getElementById('player').innerHTML = pnm
+    document.getElementById('mainimg').setAttribute('src', pid + '.jpeg')
+    document.getElementById('mainimg').style.visibility = 'visible'
+    setTimeout(function() {
+      window.open(vurl, '_blank');
+    }, 500);
+  }, 500);
+}
